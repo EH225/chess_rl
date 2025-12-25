add mixed precision floats to training pipline, correct loss function magnitude

core/base_components.py
@@ -14,19 +14,19 @@ import time, chess
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-import dask
 from dask.distributed import Client
 from itertools import batched
 from tqdm.contrib.logging import logging_redirect_tqdm
 from typing import Callable, List, Tuple, Union, Dict
 
 import torch
+import torch.nn as nn
+
 from torch.utils.tensorboard import SummaryWriter
 from collections import deque
 from utils.general import get_logger, Progbar
 from utils.replay_buffer import ReplayBuffer
-from utils.chess_env import ChessEnv, save_recording, save_move_stack, material_diff, create_ep_record, \
-    move_stack_to_states
+from utils.chess_env import ChessEnv, save_recording, save_move_stack, create_ep_record, move_stack_to_states
 from utils.general import save_eval_scores
 from utils.evaluate import evaluate_agent_game
 import core.search_algos as search_algos
@@ -178,13 +178,11 @@ class DVN:
         else:  # Otherwise if we're not using pre-trained weights, then initialize them randomly
             self.logger.info("Initializing parameters randomly")
 
-            def init_weights_randomly(m):
-                if hasattr(m, "weight"):
-                    torch.nn.init.xavier_uniform_(m.weight, gain=2 ** (1.0 / 2))
-                if hasattr(m, "bias"):
-                    torch.nn.init.zeros_(m.bias)
+            def init_random_weights(m):
+                if isinstance(m, (nn.Linear, nn.Conv2d)):
+                    nn.init.kaiming_uniform_(m.weight, a=0.01, mode='fan_in', nonlinearity='leaky_relu')
 
-            self.v_network.apply(init_weights_randomly)
+            self.v_network.apply(init_random_weights)
 
         # 3). Now that we have the model and weights initialized, move them to the appropriate device
         self.v_network = self.v_network.to(self.device)
@@ -381,7 +379,6 @@ class DVN:
         results are collected and returned in a np.ndarray. This function is designed to be called in parallel
         using dask.
 
-        :param state_batch: A batch of FEN states as a list of strings.
         :param config: A config dictionary read from yaml that specifies hyperparameters.
         :return: A np.ndarray of the same size as state_batch with state value estimates for each.
         """
@@ -420,7 +417,7 @@ class DVN:
         msg = "v_search_est expected to be 1 dimensional, got {v_search_est.shape}"
         assert len(v_search_est.shape) == 1, msg
         td_errors = (v_est - v_search_est).abs()  # Compute the absolute value TD errors
-        loss = (wts * td_errors.pow(2)).mean()  # Compute the weighted MSE loss function for all batch obs
+        loss = (wts * td_errors.pow(2)).sum()  # Compute the weighted MSE loss function for all batch obs
         # After computing the loss, we should detach the td_errors from the gradient tracking computational
         # graph so that when we make updates to the replay buffer, gradients aren't being tracked there
         return loss, td_errors.detach().cpu().numpy()  # (torch.float, np.ndarray of size (batch_size, ))
@@ -724,7 +721,8 @@ class DVN:
         # 4). Run a forward pass of the batch of states through the v_network and generate value estimates
         # i.e. what the v_network estimates is the discounted future return of following an optimal policy
         # from each state as the initial starting state
-        v_est = self.v_network(state_batch).reshape(-1)  # Returns a torch.Tensor on self.device
+        with torch.autocast(device_type=self.device, dtype=torch.bfloat16):  # Use BFloat16
+            v_est = self.v_network(state_batch).reshape(-1)  # Returns a torch.Tensor on self.device
 
         # 5). Compute the TD target values using a search function to get more accurate values by looking
         # ahead, we want to train the network to estimate this search function in the forward pass
@@ -741,7 +739,13 @@ class DVN:
         wts = torch.from_numpy(wts).to(self.device)
         v_search_estimates = torch.from_numpy(v_search_estimates).to(self.device)
 
+        print("v_est", v_est)
+        print("v_search_estimates", v_search_estimates)
+        print("wts", wts)
+        # print("regular MSE", ((v_est - v_search_estimates)**2).mean())
         loss, td_errors = self.calc_loss(v_est, v_search_estimates, wts)  # td_errors is a np.array
+        # print("td_errors", td_errors)
+        print("loss", loss)
         replay_buffer.update_priorities(indices, td_errors)  # Update the priorities for the obs sampled
         loss.backward()  # Compute gradients wrt to the trainable parameters of self.v_network
 
@@ -771,7 +775,7 @@ class DVN:
         :param verbose: Indicates whether the results of the evaluation run should be reported via logging.
         :return: The average per episode socre over num_episodes.
         """
-        self.logger.info(f"\nRunning Evaluation for {num_episodes} episodes")
+        self.logger.info(f"\nRunning Evaluation for {num_episodes} episode(s)")
         max_moves = self.config["model_training"]["max_eval_moves"]
         all_losses = []
         states = []  # If return_states is True, then also record the states generated during play

core/search_algos.py
@@ -138,7 +138,8 @@ def naive_search(state: str, model, batch_size: int = 64, gamma: float = 1.0, **
     # Compute the bootstrap value estimates in batches for the next possible states after each action
     value_estimates = []
     for state_batch in state_batches:  # Compute the value estimates in batches to minimize runtime
-        with torch.no_grad():  # Disable grad-tracking, not needed since no gradient step being taken
+        # Disable grad-tracking, not needed since no gradient step being taken, use bfloat16 dtypes
+        with torch.no_grad(), torch.autocast(device_type=model.device, dtype=torch.bfloat16):
             v_est = model(state_batch).cpu().reshape(-1).tolist()
         value_estimates.extend(v_est)  # Aggregate the state value estimates into 1 linear list
 
@@ -277,7 +278,7 @@ def minimax_search(state: str, model, gamma: float = 1.0, batch_size: int = 64, 
         # placeholder since an integer is expected to be returned
         return 9999, root.reward, np.zeros(0)
     elif horizon == 0:  # If the horizon is zero, then use the model to evaluate and return that value
-        with torch.no_grad():
+        with torch.no_grad(), torch.autocast(device_type=model.device, dtype=torch.bfloat16):
             vale_est = model([state]).cpu().reshape(-1).tolist()[0]
         return 9999, vale_est, np.zeros(0)
 
@@ -345,7 +346,7 @@ def minimax_search(state: str, model, gamma: float = 1.0, batch_size: int = 64, 
             # If the eval_batch has reached batch_size or if the node_stack for further exploration is
             # depleted, then evaluate the nodes contained in eval_batch and update the tree accordingly
             state_batch = [node.state for node in eval_batch]  # Extract a list of FEN state encodings (str)
-            with torch.no_grad():  # Compute the state estimates according to the model
+            with torch.no_grad(), torch.autocast(device_type=model.device, dtype=torch.bfloat16):
                 value_batch = model(state_batch).cpu().reshape(-1).tolist()
 
             for node, value in zip(eval_batch, value_batch):  # Update the tree with these value estimates
@@ -605,7 +606,7 @@ def monte_carlo_tree_search(state: str, model, batch_size: int = 32, n_iters: in
                 cache[leaf_node.state_] = None  # Add a placeholder in the cache, this will prevent us from
                 # running duplicative states through the model for leaf nodes within the current leaf batch
 
-        with torch.no_grad():
+        with torch.no_grad(), torch.autocast(device_type=model.device, dtype=torch.bfloat16):
             value_batch = model(state_batch).cpu().reshape(-1).tolist()  # Run in parallel on the GPU
         for idx, val_est in zip(leaf_node_idx, value_batch):  # Update the cache with the new model outputs
             cache[leaf_nodes[idx].state_] = val_est

core/torch_models.py
@@ -74,7 +74,7 @@ class MLP(nn.Module):
             nn.Linear(128, 1),
             nn.Tanh()
         )  # Use a final Tanh activation function at the end to produce value estimates [-1, +1]
-        self.device = next(self.model.parameters()).device
+        self.device = next(self.model.parameters()).device.type
 
     def state_to_model_input(self, state_batch: List[str]) -> torch.Tensor:
         """
@@ -99,7 +99,7 @@ class MLP(nn.Module):
         """
         sym_to_int = {s: i for i, s in enumerate("pnbrqk")}  # Mapping from symbol e.g. "b" to index e.g. 2
         output = torch.zeros((len(state_batch), 8, 8, 6))  # 8 rows, 8 cols, 6 piece types, encode -1, 0, or 1
-        extra_info = torch.zeros((len(state_batch), 5))  # 4 castling rights + 1 repition counter
+        extra_info = torch.zeros((len(state_batch), 5))  # 4 castling rights + 1 repetition counter
         for i, state in enumerate(state_batch):
             board = chess.Board(state)  # Use the FEN string encoding to create the board
             for cell, piece in board.piece_map().items():  # Iter over the piece dictionary
@@ -149,7 +149,7 @@ class MLP(nn.Module):
         """
         if len(state_batch) > 0:
             # Convert the input board into the expected state representation and pass it through the network
-            return self.model(self.state_to_model_input(state_batch))
+            return self.model(self.state_to_model_input(state_batch)).squeeze(1)
         else:  # If an empty batch is passed, return an empty torch.Tensor
             return torch.zeros(0).to(self.device)
 
@@ -167,12 +167,14 @@ class ResBlockCNN(nn.Module):
     def __init__(self, channels: int):
         super().__init__()
         self.conv1 = nn.Conv2d(channels, channels, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(channels)
         self.conv2 = nn.Conv2d(channels, channels, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(channels)
         self.activation = nn.LeakyReLU()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        h = self.activation(self.conv1(x))
-        h = self.conv2(h)
+        h = self.activation(self.bn1(self.conv1(x)))
+        h = self.bn2(self.conv2(h))
         return self.activation(x + h)
 
 
@@ -194,11 +196,13 @@ class CNN(nn.Module):
         self.model = nn.Sequential(
             # Conv2d Block 1
             nn.Conv2d(in_channels=17, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.LeakyReLU(),  # (batch_size, 64, 8, 8)
             ResBlockCNN(64),  # (batch_size, 64, 8, 8)
 
             # Conv2d Block 2
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.LeakyReLU(),  # (batch_size, 128, 8, 8)
             ResBlockCNN(128),  # (batch_size, 128, 8, 8)
 
@@ -211,7 +215,7 @@ class CNN(nn.Module):
             nn.Linear(128, 1),  # (batch_size, 128) -> (batch_size, 1)
             nn.Tanh()
         )  # Use a final Tanh activation function at the end to produce value estimates [-1, +1]
-        self.device = next(self.model.parameters()).device
+        self.device = next(self.model.parameters()).device.type
 
     def state_to_model_input(self, state_batch: List[str]) -> torch.Tensor:
         """
@@ -293,7 +297,7 @@ class CNN(nn.Module):
         """
         if len(state_batch) > 0:
             # Convert the input board into the expected state representation and pass it through the network
-            return self.model(self.state_to_model_input(state_batch))
+            return self.model(self.state_to_model_input(state_batch)).squeeze(1)
         else:  # If an empty batch is passed, return an empty torch.Tensor
             return torch.zeros(0).to(self.device)
 
@@ -340,7 +344,7 @@ class Transformer(nn.Module):
         self.encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=self.num_layers,
                                              norm=nn.LayerNorm(self.hidden_size))
         self.proj = nn.Linear(self.embed_size, 1)  # Final linear projection after pooling to 1 output value
-        self.device = next(self.encoder.parameters()).device
+        self.device = next(self.encoder.parameters()).device.type
 
     def state_to_model_input(self, state_batch: List[str]) -> torch.Tensor:
         """
@@ -430,7 +434,8 @@ class Transformer(nn.Module):
             x = self.encoder(x)  # Pass x through the encoder blocks, (batch_size, 68, embed_size)
             # Perform global average pooling across all the output attention vectors to get a final vector
             x = x.mean(axis=1)  # (batch_size, 68, embed_size) -> (batch_size, embed_size)
-            return F.tanh(self.proj(x))  # Linear projection to 1 final output value estimate [-1, +1]
+            # Linear projection to 1 final output value estimate [-1, +1]
+            return F.tanh(self.proj(x)).squeeze(1)
         else:  # If an empty batch is passed, return an empty torch.Tensor
             return torch.zeros(0).to(self.device)
 
@@ -472,8 +477,9 @@ class ChessAgent(DVN):
         self.v_network = model_class(self.config)  # Init the value network using the config file
 
         # Init the optimizer for training, add L2 regularization through the weight_decay parameter
-        self.optimizer = torch.optim.Adam(self.v_network.parameters(),
-                                          weight_decay=self.config["hyper_params"]["wt_decay"])
+        self.optimizer = torch.optim.AdamW(self.v_network.parameters(),
+                                           lr=self.config["hyper_params"]["lr_begin"],
+                                           weight_decay=self.config["hyper_params"]["wt_decay"])
 
     def forward(self, state_batch: List[str]) -> torch.Tensor:
         """

run.py
@@ -19,11 +19,13 @@ import sys, os
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, CURRENT_DIR)
 
-import argparse, shutil
+import argparse, shutil, torch
 from utils.general import read_yaml
 from utils.chess_env import ChessEnv
 from core.base_components import LinearSchedule
 from core.torch_models import ChessAgent
+
+torch.set_float32_matmul_precision("high")  # Enables bf16 GEMMs on GPUs that support it
 
 def run_model_training(config_name: str) -> None:
     """