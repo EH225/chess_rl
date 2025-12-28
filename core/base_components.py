fixed some sign flip issues in MCTS, updated the prior heuristic as well

core/base_components.py
@@ -17,6 +17,7 @@ from dask.distributed import Client, LocalCluster
 from itertools import batched
 from tqdm.contrib.logging import logging_redirect_tqdm
 from typing import Callable, List, Tuple, Union, Dict, Optional
+import logging
 
 import torch
 import torch.nn as nn
@@ -28,6 +29,7 @@ from utils.chess_env import ChessEnv, save_recording, save_move_stack, create_ep
 from utils.evaluate import evaluate_agent_game
 import core.search_algos as search_algos
 
+logging.getLogger("distributed.utils").setLevel(logging.ERROR)
 
 def setup_path(dask_worker):
     """
@@ -286,12 +288,6 @@ class DVN:
 
         # 2). Split the input state_batch into equal parts according to the number of threads in the cluster
         nthreads = self.dask_client.scheduler_info()["total_threads"]
-        nthreads1 = sum([w.nthreads for w in self.dask_client.cluster.workers.values()])
-        nthreads2 = sum(w["nthreads"] for w in self.dask_client.scheduler_info()["workers"].values())
-        print("nthreads", nthreads) ### TODO: See how many threads this line things there is
-        print("nthreads1", nthreads1)
-        print("nthreads2", nthreads2)
-
         # nthreads = sum([worker.nthreads for worker in self.dask_client.cluster.workers.values()])
         state_batches = list(batched(state_batch, max(1, len(state_batch) // nthreads)))  # Equal parts
 
@@ -535,12 +531,12 @@ class DVN:
         # Final screen updates
         self.logger.info("Training Complete!")
         self.save()  # Save the final model weights after training has finished
-        self.dask_client.shutdown()  # Shutdown the dask cluster after training has finished
         # Run another evaluation episode at the very end, this will auto log the results to the CSV history
         score, states = self.evaluate(num_episodes=self.config["model_training"]["num_episodes_test"],
                                       record_last=self.record, return_states=True)
         duration = time.time() - global_start_time
         self.logger.info(f"Training finished in {duration / 60:.1f} minutes")
+        self.dask_client.shutdown()  # Shutdown the dask cluster after training has finished
 
     def update_step(self, replay_buffer: ReplayBuffer, lr: float, beta: float, t: int) -> Tuple[int, int]:
         """
@@ -579,7 +575,7 @@ class DVN:
         v_est_np = v_est.detach().to(torch.float32).cpu().numpy() # Convert over to numpy for reporting
         summary_stats = [f"{x:.2f}" for x in [v_est_np.max(), v_est_np.min(), v_est_np.mean(),
                                               np.abs(v_est_np).mean(), v_est_np.std()]]
-        summary_stats = "(max, min, mean, |mean|, std) = (" + ", ".join(summary_stats) + ")"
+        summary_stats = "(max, min, mean, abs mean, std) = (" + ", ".join(summary_stats) + ")"
         self.logger.info(f"\t   v_est: {summary_stats}")
         v_est_np_model = v_est_np  # Save the alias
 
@@ -592,7 +588,7 @@ class DVN:
         v_est_np = v_search_estimates.copy()
         summary_stats = [f"{x:.2f}" for x in [v_est_np.max(), v_est_np.min(), v_est_np.mean(),
                                               np.abs(v_est_np).mean(), v_est_np.std()]]
-        summary_stats = "(max, min, mean, |mean|, std) = (" + ", ".join(summary_stats) + ")"
+        summary_stats = "(max, min, mean, abs mean, std) = (" + ", ".join(summary_stats) + ")"
         corr = np.corrcoef(v_est_np_model, v_est_np)[0, 1] # Compute the correlation of the ys and yhats
         self.logger.info(f"\t   v_est: {summary_stats}, corr: {corr:.2f}")
 

core/search_algos.py
@@ -12,7 +12,7 @@ sys.path.insert(0, PARENT_DIR)
 
 import numpy as np
 import torch, chess
-from utils.chess_env import ChessEnv
+from utils.chess_env import ChessEnv, relative_material_diff
 from scipy.special import softmax
 from typing import Tuple, List, Callable, Optional
 
@@ -451,9 +451,16 @@ class Node_MCTS:
             else:
                 n_eff = child.n_visits + child.virtual_loss  # virtual_loss discourages repeat selections
                 val = child.value_sum / n_eff if n_eff > 0 else 0  # Avoid division by 0 issues
+                # At this node, we will select the the child whose value is most appealing to this node's
+                # perspective, all value_sums are from the root's perspective so flip the signs if this node
+                # is played by the opponent who would want to minimize the return of the root node
+                val *= (1 if self.node_turn == self.root_turn else -1)
                 c_puct = self.c_init + np.log(n_eff + self.c_base + 1) - np.log(self.c_base)
+                # child.prior is recorded from the perspective of the this node, the parent node so no sign
+                # flip is needed, they're also normalized using softmax so it's non-trivial to flip
                 val += c_puct * child.prior * np.sqrt(self.n_visits) / (1 + n_eff)
-                if val > max_val:  # Update the argmax node seen so far
+                if val > max_val:  # Update the argmax node seen so far, select the highest value node among
+                    # all child nodes from this node's turn perspective
                     argmax_node, max_val = child, val
 
         return argmax_node
@@ -484,9 +491,10 @@ class Node_MCTS:
                 if prior_heuristic is None:  # If no prior_heuristic provided, then set to the unif prior 1/n
                     child.prior = uniform_prior
                 else:  # If a prior_heuristic is given, then use it to generate a prior for each child node
-                    # Flip sign if eval from other side so that all are from the perspective of the root node
-                    sign_flip = (1 if child.node_turn == child.root_turn else -1)
-                    child.prior = prior_heuristic(child.state) * sign_flip
+                    # The prior_heuristic will be evaluated from the perspective of the child i.e. the player
+                    # to go next in the child node state, flip the sign since it will only be accessed by
+                    # the parent node in making an expansion selection, will be passed through softmax
+                    child.prior = prior_heuristic(child.state) * (-1)
                 self.children.append(child)
                 board.pop()  # Backtrack to visit the next legal move
                 unvisited_leaf_nodes_chg += 1  # Record another unexplored child node added
@@ -546,7 +554,7 @@ class Node_MCTS:
 
 
 def monte_carlo_tree_search(state: str, model, batch_size: int = 32, n_iters: int = 200,
-                            prior_heuristic: Callable = material_heuristic,
+                            prior_heuristic: Callable = relative_material_diff,
                             **kwargs) -> Tuple[int, float, np.ndarray]:
     """
     Performs Monte Carlo Tree Search (MCTS) for n iterations where model is used as a state value approximator

core/torch_models.py
@@ -17,7 +17,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from core.base_components import DVN
 import core.search_algos as search_algos
-from utils.chess_env import ChessEnv, create_ep_record
+from utils.chess_env import ChessEnv, create_ep_record, relative_material_diff
 from typing import Tuple, List, Dict
 
 ##################################################
@@ -25,51 +25,10 @@ from typing import Tuple, List, Dict
 ##################################################
 # TODO: Section marker
 
-
 class MaterialHeuristic(nn.Module):
     def __init__(self):
         super().__init__()
         self.device = "cpu"  # Alwys run on the CPU
-
-    @staticmethod
-    def relative_material_diff(state: str) -> float:
-        """
-        Computes the net relative material difference of the current board state from the perpsective of the
-        player who is to move next where each piece is worth:
-            pawn (0.125), knight (3), bishop (3), rook (5), queen (9), king (0)
-
-        The relative material difference is defined as:
-            (player material) - (opponent material)
-
-        This net material difference is then normalized by min(total_material / 2, 10) where we cap the
-        denominator scaling by 10 so that being up a queen or 2 rooks is almost certainly going to win the
-        game. If the current player is in check, then subtract 0.1 from the score for them, being in check is
-        a vulnerable position and restricts the moves of the player moving next, therefore it should decrease
-        the overall expected reward for the perspective of the player to move next. Final values are clipped
-        at [-1, +1] after this check penality is added.
-
-        :param state: A FEN string denoting the current game state.
-        :return: A float value estimate of the game outcome based on relative material.
-        """
-        board = chess.Board(state)  # Convert to a chess.Board object to access the piece map
-        if board.is_game_over(): # If the game is in an end state, return the terminal reward
-            return -1.0 if board.is_checkmate() else 0.0
-
-        piece_values = {"p": 0.125, "n": 3, "b": 3, "r": 5, "q": 9, "k": 0}
-        total_material, player_material = 0, 0
-        for p in board.piece_map().values():
-            piece_val = piece_values[p.symbol().lower()]  # Get the absolute value of each piece
-            if board.turn is True and p.symbol().isupper():  # For white, upper-case pieces are friendly
-                player_material += piece_val
-            elif board.turn is False and p.symbol().islower():  # For black, lower-case pieces are friendly
-                player_material += piece_val
-            total_material += piece_val
-
-        net_material = player_material - (total_material - player_material)
-        denom = min(total_material / 2, 10)  # Scaling context for the net_material diff, set it to be half
-        # the total material on the board or at most 20 i.e. being up a queen is 90% going to result in you
-        # winning the game
-        return np.clip(net_material / denom + (-0.2 if board.is_check() else 0), -1, 1)
 
     def forward(self, state_batch: List[str]) -> torch.Tensor:
         """
@@ -81,7 +40,7 @@ class MaterialHeuristic(nn.Module):
         :return: A torch.Tensor of size (batch_size, ) with the value estimates for each stating position.
         """
         if len(state_batch) > 0:
-            return torch.Tensor([self.relative_material_diff(state) for state in state_batch])
+            return torch.Tensor([relative_material_diff(state) for state in state_batch])
         else:  # If an empty batch is passed, return an empty torch.Tensor
             return torch.zeros(0).to(self.device)
 